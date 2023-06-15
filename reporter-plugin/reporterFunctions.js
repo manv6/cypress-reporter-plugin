57bd2c950148f36537507be28c0b87646343bfe9
manv6 <evlastos@gmail.com>
2023-06-15 15:47:00 +0300

    FIX - prevent circular reference from object when use stringify

--- reporter-plugin/reporterFunctions.js
+++ reporter-plugin/reporterFunctions.js
@@ -209,6 +209,23 @@ function replaceUrls(baseUrl, style) {
   };
 }
 
+function removeCircularRef(obj) {
+  let cache = [];
+  let str = JSON.stringify(obj, function(key, value) {
+    if (typeof value === "object" && value !== null) {
+      if (cache.indexOf(value) !== -1) {
+        // Circular reference found, discard key
+        return;
+      }
+      // Store value in our collection
+      cache.push(value);
+    }
+    return value;
+  });
+  cache = null; // reset the cache
+  return str;
+}
+
 module.exports = {
   getHTML,
   replaceUrls,
@@ -220,4 +237,5 @@ module.exports = {
   generateSnapMetaData,
   grabInteractedElements,
   updateLastSnapshotProperties,
+  removeCircularRef
 };
